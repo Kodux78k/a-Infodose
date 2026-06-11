ed617b357dd44ff05ac0e2261a2f3dc9213db472
Kodux78k <koduxlive@gmail.com>
2026-06-11 00:30:50 -0300

    Update main.js

--- oiDual/KODUX/78K/APPS/a€Oidual/js/main.js
+++ oiDual/KODUX/78K/APPS/a€Oidual/js/main.js
@@ -1,6 +1,6 @@
 console.log("Infodose conectado", {"ts":1781146340473,"id":"a8786b72-30f7-4f38-a9bb-2c01a422ce0b","meta":{"app":"generated.app"}});
 import "./modules/inline-0.js";
-import "./modules/inline-1.js";
+
 import "./modules/inline-2.js";
 import "./modules/inline-3.js";
 import "./modules/inline-4.js";
