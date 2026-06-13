036e85d3189cba336fe18c45532777a9828e99e3
Kodux78k <koduxlive@gmail.com>
2026-06-13 18:39:24 -0300

    Atualizar o main.js

--- js/main.js
+++ js/main.js
@@ -12,13 +12,13 @@ import './modules/inline-3.js';
 import './modules/inline-4.js';
 import './modules/inline-5.js';
 import './modules/KOB-RHEA-KAOS-sync.js';
-import './modules/kob.js';
+import './modules/kob2.js';
 import './modules/inline-7-9.js';
 import './modules/inline-8.js';
 import './modules/inline-9.js';
 import './modules/inline-10.js';
 import './modules/inline-11.js';
-import './modules/a€Arx.js';
+import './a€Arx.js';
 console.log(`
 ╔════════════════════════════════════╗
 ║ KBllX  READY                       ║
