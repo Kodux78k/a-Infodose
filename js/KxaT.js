bf8947a7d3eabc49b998a40ba6337c293efdd37a
Kodux78k <koduxlive@gmail.com>
2026-06-19 05:19:42 -0300

    Update KxaT.js

--- js/KxaT.js
+++ js/KxaT.js
@@ -4,4 +4,4 @@ console.log("[RL] ID da sessão:", "348fab2c-a5ef-4d12-8e5b-3fde8577db6a");
 console.log("[RL] Aplicação:", "generated.app");
 
 import "https://kodux78k.github.io/oiDual--Y-/M0D/KxaT/js/main.js";
-import "./modules/archz.js";
+import "https://www.infodose.com.br/js/modules/archz.js";
