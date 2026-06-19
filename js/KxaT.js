fea14e5ebd563a191303c18bbea572e4b775a9f1
Kodux78k <koduxlive@gmail.com>
2026-06-19 05:10:24 -0300

    Add import for archz.js module

--- js/KxaT.js
+++ js/KxaT.js
@@ -1,4 +1,4 @@
 console.log("Infodose conectado", {"ts":1778715871351,"id":"348fab2c-a5ef-4d12-8e5b-3fde8577db6a","meta":{"app":"generated.app"}});
 import "https://kodux78k.github.io/oiDual--Y-/M0D/KxaT/js/main.js";
-
+import './modules/archz.js'; 
 
