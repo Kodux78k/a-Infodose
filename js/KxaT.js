80881f100500be6a04879c71b77c3dda74b528a7
Kodux78k <koduxlive@gmail.com>
2026-06-19 05:13:40 -0300

    Update KxaT.js

--- js/KxaT.js
+++ js/KxaT.js
@@ -1,4 +1,7 @@
-console.log("Infodose conectado", {"ts":1778715871351,"id":"348fab2c-a5ef-4d12-8e5b-3fde8577db6a","meta":{"app":"generated.app"}});
-import "https://kodux78k.github.io/oiDual--Y-/M0D/KxaT/js/main.js";
-import './modules/archz.js'; 
+console.log("[RL] Infodose conectado");
+console.log("[RL] Timestamp:", 1778715871351);
+console.log("[RL] ID da sessão:", "348fab2c-a5ef-4d12-8e5b-3fde8577db6a");
+console.log("[RL] Aplicação:", "generated.app");
 
+import "https://kodux78k.github.io/oiDual--Y-/M0D/KxaT/js/main.js";
+import "./modules/archz.js";
