bb34b60f28e9b941a5d551cb42ec2effa358e63b
Kodux78k <koduxlive@gmail.com>
2026-06-20 18:40:03 -0300

    Atualizar o main.js

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ import './modules/inline-7-9.js';
 import './modules/inline-8.js';
 import './modules/inline-9.js';
 import './modules/inline-10.js';
-import './modules/inline-11.js';
+/*import './modules/inline-11.js';*/
 
 console.log(`
 ╔════════════════════════════════════╗
