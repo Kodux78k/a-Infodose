fdb7fa5087a0c8188fa3fd71e21a7e33646e9a65
Kodux78k <koduxlive@gmail.com>
2026-06-30 13:37:43 -0300

    Create modules.js

---
+++ oiDual/KODUX/78K/APPS/78HU/js/modules.js
@@ -0,0 +1,17 @@
+console.log("Infodose conectado", {
+  ts: 1771291932319,
+  id: "26fbbf40-793d-4a9a-9a44-5d395a462def",
+  meta: {
+    app: "generated.app"
+  }
+});
+
+import "https://www.infodose.com.br/oiDual/KODUX/78K/APPS/78HU/js/modules/inline-0.js";
+import "https://www.infodose.com.br/oiDual/KODUX/78K/APPS/78HU/js/modules/inline-1.js";
+import "https://www.infodose.com.br/oiDual/KODUX/78K/APPS/78HU/js/modules/inline-2.js";
+import "https://www.infodose.com.br/oiDual/KODUX/78K/APPS/78HU/js/modules/inline-3.js";
+import "https://www.infodose.com.br/oiDual/KODUX/78K/APPS/78HU/js/modules/inline-4.js";
+import "https://www.infodose.com.br/oiDual/KODUX/78K/APPS/78HU/js/modules/inline-5.js";
+import "https://www.infodose.com.br/oiDual/KODUX/78K/APPS/78HU/js/modules/inline-6.js";
+import "https://www.infodose.com.br/oiDual/KODUX/78K/APPS/78HU/js/modules/inline-7.js";
+import "https://www.infodose.com.br/oiDual/KODUX/78K/APPS/78HU/js/modules/inline-8.js";
