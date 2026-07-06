d84daad2d15327d0d65557dca623706ed5e41b3f
Kodux78k <koduxlive@gmail.com>
2026-07-05 21:04:01 -0300

    Criar main-369.js

---
+++ js/main-369.js
@@ -0,0 +1,31 @@
+// Gerado automaticamente
+import { makeCollapsible } from "./modules/motor-9-369.js";
+import { generateFractals } from "./modules/motor-9-369.js";
+import { showToast } from "./modules/motor-9-369.js";
+import { update78K } from "./modules/motor-9-369.js";
+import { di_getSequence } from "./modules/motor-9-369.js";
+import { populateArchSelect } from "./modules/motor-9-369.js";
+
+// Inicialização
+document.addEventListener('DOMContentLoaded', () => {
+    makeCollapsible();
+    populateArchSelect();
+    update78K();
+});
+
+
+/*
+// Gerado automaticamente
+import { makeCollapsible } from "./modules/accordion.js";
+import { generateFractals } from "./modules/fractals.js";
+import { showToast } from "./modules/toast.js";
+import { update78K } from "./modules/engine78k.js";
+import { di_getSequence } from "./modules/engine369.js";
+import { populateArchSelect } from "./modules/archetypes.js";
+
+// Inicialização
+document.addEventListener('DOMContentLoaded', () => {
+    makeCollapsible();
+    populateArchSelect();
+    update78K();
+});*/
