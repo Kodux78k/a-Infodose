84e3b079c6ce0cf25ac6fe26d3197dc746320c0f
Kodux78k <koduxlive@gmail.com>
2026-07-06 04:35:49 -0300

    Add files via upload

---
+++ oiDual/KODUX/78K/APPS/78KARD/js/main.js
@@ -0,0 +1,5 @@
+console.log("Infodose conectado", {"ts":1783323124589,"id":"d2440fdc-fe10-471a-bbef-110ddb923c0a","meta":{"app":"generated.app"}});
+import "./modules/inline-0.js";
+import "./modules/inline-1.js";
+import "./modules/inline-2.js";
+import "./modules/inline-3.js";
