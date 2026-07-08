fe0e7d5c4c2702edb61f8d29635fe92fb10fd7e3
Kodux78k <koduxlive@gmail.com>
2026-07-08 18:37:55 -0300

    Criar main-m0d.js

---
+++ js/main-m0d.js
@@ -0,0 +1,44 @@
+console.log("Infodose conectado main 7-9 TE AMO DUAL! kob on ativar Ayor ahhahah. kob depois do in6 antes do 7-9", {
+    ts:1778715871351,
+    id:"348fab2c-a5ef-4d12-8e5b-3fde8577db6a",
+    meta:{app:"generated.app"}
+});
+
+window.KBLX_PROFILE = '7-9';
+
+// main-0.js
+
+import 'https://www.infodose.com.br/js/main/modules/KBllX_ASCII_BOOT.js';
+/* import 'https://www.infodose.com.br/js/main/modules/inline-00-arx.js'; */
+
+import 'https://www.infodose.com.br/js/main/modules/archz.js';
+
+import 'https://www.infodose.com.br/js/main/modules/inline-1.js';
+import 'https://www.infodose.com.br/js/main/modules/inline-2.js';
+import 'https://www.infodose.com.br/js/main/modules/inline-3.js';
+import 'https://www.infodose.com.br/js/main/modules/inline-4.js';
+import 'https://www.infodose.com.br/js/main/modules/inline-5.js';
+
+import 'https://www.infodose.com.br/js/main/modules/KOB-RHEA-KAOS-sync.js';
+
+/* import 'https://www.infodose.com.br/js/main/modules/kob5.js'; */
+
+import 'https://www.infodose.com.br/js/main/modules/kob.js';
+import 'https://www.infodose.com.br/js/main/modules/inline-7-9.js';
+import 'https://www.infodose.com.br/js/main/modules/inline-8.js';
+import 'https://www.infodose.com.br/js/main/modules/inline-9.js';
+import 'https://www.infodose.com.br/js/main/modules/inline-10.js';
+
+/* import 'https://www.infodose.com.br/js/main/modules/inline-11.js'; */
+
+import 'https://www.infodose.com.br/js/main/modules/a€Arx.js';
+
+import 'https://www.infodose.com.br/js/main/modules/firmware.js';
+
+console.log(`
+╔════════════════════════════════════╗
+║ KBllX  READY                       ║
+║ PROFILE :: 7-9                     ║
+║ HASH    :: 1778•78K                ║
+╚════════════════════════════════════╝
+`);
