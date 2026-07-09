8cdf310c07f126c77d88f50eb32b01a64ac9ff6d
Kodux78k <koduxlive@gmail.com>
2026-07-09 03:40:12 -0300

    Atualizar o response_handler.js

--- KOBLLUX_MODULAR_0/ESPIRITO/ui/response_handler.js
+++ KOBLLUX_MODULAR_0/ESPIRITO/ui/response_handler.js
@@ -29,7 +29,7 @@ export class ResponseHandler {
                         <button class="control-btn paste-button" title="Colar">
                             <svg viewBox="0 0 24 24" width="20"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="4" x2="12" y2="20"></line></svg>
                         </button>
-                        <button id="toggleBtn" class="control-btn toggle-button" title="Check Connection & Training">
+                        <button id="toggleBtnF" class="control-btn toggle-button" title="Check Connection & Training">
                             <svg viewBox="0 0 24 24" width="20" height="20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M18.36 6.64a9 9 0 1 1-12.73 0"></path>
                                 <line x1="12" y1="2" x2="12" y2="12"></line>
